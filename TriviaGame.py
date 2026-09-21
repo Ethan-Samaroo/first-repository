2755e72b9b240c95f9fa94b7c734b34740f6106b
Ethan-Samaroo <esamaroo21k@gmail.com>
2026-09-21 14:41:48 -0400

    Working trivia game

--- TriviaGame.py
+++ TriviaGame.py
@@ -5,27 +5,40 @@ def menu():
           "3.View High Score\n"
           "4. Exit")
     option = input("Select an option: ")
+    return option
 
 
-def start_game():
-    asking_question()
+categories = {"What's my name?": "General"}
 
 
 def asking_question():
     points = 0
-    choices = {1: "A. Ethan \n B. Aidan \n C.Haseeb \n D.Hitesh"}
-    questions = {1: "What's my name?"}
-    answers = {1: "A"}
-    for index, question in enumerate(questions):
-        print(questions[index + 1])
-        print(choices[index + 1])
-        answer = index("Choose an answer: ")
-        if answer.upper() == answer[index + 1]:
-            point += 5
+    choices = {"What's my name?": "A. Ethan \n B. Aidan \n C.Haseeb \n D.Hitesh"}
+    answers = {"What's my name?": "A"}
+    for question in choices:
+        print(question)
+        print(choices[question])
+        answer = input("Choose an answer: ")
+        if answer.upper() == answers[question]:
+            points += 5
+    print("Your score: " + str(points))
+    return points
 
 
-option = 0
-while option != 4:
-    menu()
-    if option == "1:":
-        start_game()
+highscore = 0
+option = menu()
+
+while option != "4":
+    if option == "1":
+        score = asking_question()
+        if highscore < score:
+            print("NEW HIGH SCORE!!!")
+            highscore = score
+    if option == "2":
+        for category in categories:
+            print(categories[category])
+    if option == "3":
+
+        print("High Score: " + str(highscore))
+
+    option = menu()
