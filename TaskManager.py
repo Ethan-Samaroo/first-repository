ed02bda21b7edd0d06f48705cbaf8eabc1006d7b
Ethan-Samaroo <esamaroo21k@gmail.com>
2026-09-25 16:29:27 -0400

    updated task manager

--- TaskManager.py
+++ TaskManager.py
@@ -10,4 +10,27 @@ def menu() -> int:
     "7. Sort Tasks \n" \
     "8. Find Task \n"\
     "9. Exit \n")
-    
+    try:
+        option = int(input("Please select an option: "))
+    except ValueError:
+        print("Please input a number!")
+    return option 
+def add_task():
+    input = input("Add a task: ")
+    tasks.append(input)
+    print("Task Added")
+def view_tasks():
+    for index, task in enumerate(tasks):
+        print(f"{index}. {task}/n")
+def delete_task(delete_task):
+    for task in tasks:
+        if delete_task == task:
+            tasks.remove(task)
+def search_task(certain_task)-> str:
+    for task in tasks:
+        if certain_task in task:
+            return task
+    return "Task not found"
+tasks = []
+while option != 9:
+    menu()
