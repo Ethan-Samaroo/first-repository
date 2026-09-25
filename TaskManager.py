e7223348283c755123387abacbeec5724367dbcf
Ethan-Samaroo <esamaroo21k@gmail.com>
2026-09-25 15:54:36 -0400

    started task manager project

---
+++ TaskManager.py
@@ -0,0 +1,13 @@
+def menu() -> int:
+    option = 0
+    print("====== TASK MANAGER ====== \n " \
+    "1. Add Task \n"\
+    "2. View Tasks \n"\
+    "3. Search Tasks \n"\
+    "4. Complete Task \n" \
+    "5. Delete Task \n"\
+    "6. View Priority Queue \n" \
+    "7. Sort Tasks \n" \
+    "8. Find Task \n"\
+    "9. Exit \n")
+    
